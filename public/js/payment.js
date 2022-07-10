b3ffa327ed2e54b00c7251c439e93bec4e7bd741
odo27 <odo27@naver.com>
2022-07-11 08:36:19 +0900

    payment using 'iamport'
    I added payment button on index.html (I don't know where clients want to place that button). I used 'iamport' it's required jQuery. I added requestPay function using 'iamport' sample codes.

---
+++ public/js/payment.js
@@ -0,0 +1,24 @@
+var IMP = window.IMP;
+IMP.init("imp56407262");
+
+function requestPay() {
+      // IMP.request_pay(param, callback) 결제창 호출
+      IMP.request_pay({ // param
+          pg: "html5_inicis",
+          pay_method: "card",
+          merchant_uid: "ORD20180131-0000011",
+          name: "노르웨이 회전 의자",
+          amount: 64900,
+          buyer_email: "gildong@gmail.com",
+          buyer_name: "홍길동",
+          buyer_tel: "010-4242-4242",
+          buyer_addr: "서울특별시 강남구 신사동",
+          buyer_postcode: "01181"
+      }, function (rsp) { // callback
+          if (rsp.success) {
+
+          } else {
+
+          }
+      });
+    }
