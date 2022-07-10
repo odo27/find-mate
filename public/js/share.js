548da40a7637542c90bf5e4a9c7127985706767a
odo27 <odo27@naver.com>
2022-07-11 07:50:27 +0900

    share to twitter, web share api (annotated)
    I added share to twitter and share using link in browser. But It needs https -> ssl -> domain. I made link share function using web share api, I annotated that codes because I can't get domain now.

--- public/js/share.js
+++ public/js/share.js
@@ -1,4 +1,4 @@
-const url = 'http://220.149.88.51:8080/'
+const url = 'http://220.149.88.26:8080/'
 
 function kakaoShare() {
   Kakao.Share.sendDefault({
@@ -28,3 +28,15 @@ function facebookShare() {
   }, function(response) {});
 
 }
+
+function twitterShare() {
+  window.open("https://twitter.com/intent/tweet?url=" + url, "find-mate");
+}
+
+//function linkShare() {
+//  navigator.share({
+//    title: '당신의 소울메이트는?',
+//    text: 'DA R D Field Data 기반으로 당신만의 소울메이트를 찾습니다.',
+//    url: url
+//  })
+//}
