f3f9ce61a4806a01a40b3732abae6a8f8c0bed9b
odo27 <odo27@naver.com>
2022-08-24 11:49:03 +0900

    iamport test payment
    I remove jquery js file. I revise present payment to test payment.

--- public/js/payment.js
+++ public/js/payment.js
@@ -3,24 +3,17 @@ var IMP = window.IMP;
 IMP.init("imp56407262");
 
 function requestPay() {
-      // IMP.request_pay(param, callback)
-      // calling a payment window
-      IMP.request_pay({ // param
-          pg: "html5_inicis",
-          pay_method: "card",
-          merchant_uid: "ORD20180131-0000011",
-          name: "춤추는 우주인 구독하기",
-          amount: 2900,
-          buyer_email: "gildong@gmail.com",
-          buyer_name: "홍길동",
-          buyer_tel: "010-4242-4242",
-          buyer_addr: "서울특별시 강남구 신사동",
-          buyer_postcode: "01181"
-      }, function (rsp) { // callback
-          if (rsp.success) {
-
-          } else {
-
-          }
-      });
-    }
+  IMP.request_pay({
+    pg: "html5_inicis",
+  	amount : 1000,
+  	buyer_name : '홍길동',
+  	name : '결제테스트'
+  }, function(response) {
+  	// callback function after payment
+  	if ( response.success ) { // payment success
+  		console.log(response);
+  	} else {
+  		alert('결제실패 : ' + response.error_msg);
+  	}
+  })
+}
